be092920fcb9527616f3407240e0acae1215e547
aechavarria24 <aechavarria24@misena.edu.co>
2018-01-07 17:46:03 -0500

    retoque de vistas

--- resources/views/partial/cliente.blade.php
+++ resources/views/partial/cliente.blade.php
@@ -1,20 +1,25 @@
 
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="">Documento</label>
-                    <input requied type="text" disabled name="documento" value="{{$documento}}">
-                </div>
-                <div class="form-group">
-                    <label for="">Nombres</label>
-                    <input requied type="text" disabled name="Nombre" value="{{$nombres}}">
-                </div>
-                <div class="form-group">
-                    <label for="">Apellidos</label>
-                    <input requied type="text" disabled name="Apellidos" value="{{$apellidos}}">
-                </div>
-                <div class="form-group">
-                    <label for="">Referente de pago</label>
-                    <input requied type="text" disabled name="Referente_pago" value="{{$referente_pago}}">
-                </div>
-            </div>
-                {{ $slot }}
+<div class="">
+                        <div class="form-group col-md-6 col-sm-6">
+                                                <label for="">Documento</label>
+                                                <input class="form-control" requied type="text" disabled name="documento" value="{{$documento}}">
+                        </div>
+
+
+                        <div class="form-group col-md-6 col-sm-6 ">
+                                                <label for="">Referente de pago</label>
+                                                <input class="form-control" requied type="text" disabled name="Referente_pago" value="{{$referente_pago}}">
+                        </div>
+
+                        <div class="form-group col-md-6 col-sm-6 ">
+                                                <label for="">Nombres</label>
+                                                <input class="form-control" requied type="text" disabled name="Nombre" value="{{$nombres}}">
+                        </div>
+
+                        <div class="form-group col-md-6 col-sm-6 ">
+                                                <label for="">Apellidos</label>
+                                                <input class="form-control" requied type="text" disabled name="Apellidos" value="{{$apellidos}}">
+                        </div>
+
+</div>
+{{ $slot }}
--- resources/views/pse/cliente.blade.php
+++ resources/views/pse/cliente.blade.php
@@ -1,56 +1,53 @@
 @extends('layouts.app')
 @section('title')
-
-            Facil,  rapido y seguro
-
+Facil,  rapido y seguro
 @endsection
 @section('content')
 
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title" >PSE-Pagos seguros en linea/persona natural</h3>
+    </div>
     <div class="box-body">
 
         <form id='frm_cliente' role="form" method="get" action="{{ route('bank_list') }}">
             <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <h4>PSE-Pagos seguros en linea/persona natural</h4>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group col-md-6">
-                        <div class="">
 
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </div>
+                <div class="col-md-12">
+                    <div class="form-group col-md-6 col-sm-6">
+                        <i class="fa fa-user" aria-hidden="true"></i> &nbsp;
                         <label for="">Persona</label>
                     </div>
-                    <div class="form-group col-md-6">
-                        <div class="">
-
-                            <i class="fa fa-industry" aria-hidden="true"></i>
-                        </div>
+                    <div class="form-group col-md-6 col-sm-6">
+                        <i class="fa fa-industry" aria-hidden="true"></i>&nbsp;
                         <label for="">Persona juridica</label>
                     </div>
-                    <div class="form-group col-md-6">
+                    <hr>
+                    <div class="form-group col-md-6 col-sm-6">
                         <label for="">Soy un usuario registrado</label>
                     </div>
-                    <div class="form-group col-md-6">
-
-                        <i class="fa fa-user-plus" aria-hidden="true"> Quiero registrarme ahora</i>
+                    <div class="form-group col-md-6 col-sm-6">
+                        <i class="fa fa-user-plus" aria-hidden="true"></i>&nbsp;
+                        <label for="">Quiero registrarme ahora</label>
                     </div>
-                    <div class="form-group col-md-12">
-                        <label for="" class="col-md-4">E-mail</label>
-                        <input required type="email" name="correo" value="">
+                    <div class="form-group col-md-12 col-md-12">
+                        <label for="" class="">E-mail</label>
+                        <input class="form-control" required type="email" name="correo" value="">
+                    </div>
+                    <div class="col-md-6 col-sm-6 ">
+                        <button class="btn btn-info btn-block" type="submit" id="id_abandonar_pago" name="pagar">Abandonar el pago</button>
+                    </div>
+                    <div class="col-md-6 col-sm-6 ">
+                        <button class="btn btn-info btn-block" type="button" id="id_seguir" name="pagar">Seguir</button>
                     </div>
                 </div>
-                <div class="">
-                    <button class="btn btn-info" type="submit" id="id_abandonar_pago" name="pagar">Abandonar el pago</button>
-                    <button class="btn btn-info" type="button" id="id_seguir" name="pagar">Seguir</button>
-                </div>
 
-            </form>
-        </div>
-        @section('script')
-            <script src ="/js/my_script.js/cliente/cliente.js"></script>
-        @endsection
+            </div>
+        </form>
+    </div>
+</div>
+@section('script')
+<script src ="/js/my_script.js/cliente/cliente.js"></script>
+@endsection
 
-    @endsection
+@endsection
